e1a32872d8bf2541e48045ca36a3729be49b3cf1
abykova <abykova@wooppay.com>
2019-06-10 10:38:10 +0600

    [fix] show modal

--- views/site/index.php
+++ views/site/index.php
@@ -9,76 +9,73 @@ use yii\helpers\Html;
 <div id="colorlib-page">
     <a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
     <aside id="colorlib-aside" role="complementary" class="js-fullheight text-center">
-        <h1 id="colorlib-logo"><?= Html::a('<span class="flaticon-camera"></span>Go Cafe',['site/index']) ?>
-        <nav id="colorlib-main-menu" role="navigation">
-            <ul>
-                <li class="colorlib-active"><?= Html::a('Home',['site/index']) ?></li>
-                <li><?= Html::a('About',['site/about']) ?></li>
-                <li><?= Html::a('Contact',['site/contact']) ?></li>
-                <li><input placeholder="заведение" type='text'></li>
-                <li><input placeholder="время работы"></li>
-                <li><input placeholder="кухня"></li>
-            </ul>
-            <ul class="tagcloud">
-                <a href="#" class="tag-cloud-link">кухня</a>
-                <a href="#" class="tag-cloud-link">время</a>
-                <a href="#" class="tag-cloud-link">праздник</a>
-                <a href="#" class="tag-cloud-link">акции</a>
-                <a href="#" class="tag-cloud-link">суши</a>
-                <a href="#" class="tag-cloud-link">leaves</a>
-                <a href="#" class="tag-cloud-link">food</a>
-                
-            </ul>
-        </nav>
-
-        <div class="colorlib-footer">
-            <h3>Follow Us Here!</h3>
-            <div class="d-flex justify-content-center">
-                <ul class="d-flex align-items-center">
-                    <li class="d-flex align-items-center jusitfy-content-center"><a href="#"><i
-                                    class="icon-facebook"></i></a></li>
-                    <li class="d-flex align-items-center jusitfy-content-center"><a href="#"><i
-                                    class="icon-twitter"></i></a></li>
-                    <li class="d-flex align-items-center jusitfy-content-center"><a href="#"><i
-                                    class="ifigurefigurefigurcon-instagram"></i></a></li>
-                    <li class="d-flex align-items-center jusitfy-content-center"><a href="#"><i
-                                    class="icon-linkedin"></i></a></li>
+        <h1 id="colorlib-logo"><?= Html::a('<span class="flaticon-camera"></span>Go Cafe', ['site/index']) ?>
+            <nav id="colorlib-main-menu" role="navigation">
+                <ul>
+                    <li class="colorlib-active"><?= Html::a('Home', ['site/index']) ?></li>
+                    <li><?= Html::a('About', ['site/about']) ?></li>
+                    <li><?= Html::a('Contact', ['site/contact']) ?></li>
+                    <li><input placeholder="заведение" type='text'></li>
+                    <li><input placeholder="время работы"></li>
+                    <li><input placeholder="кухня"></li>
                 </ul>
+                <ul class="tagcloud">
+                    <a href="#" class="tag-cloud-link">кухня</a>
+                    <a href="#" class="tag-cloud-link">время</a>
+                    <a href="#" class="tag-cloud-link">праздник</a>
+                    <a href="#" class="tag-cloud-link">акции</a>
+                    <a href="#" class="tag-cloud-link">суши</a>
+                    <a href="#" class="tag-cloud-link">leaves</a>
+                    <a href="#" class="tag-cloud-link">food</a>
+
+                </ul>
+            </nav>
+
+            <div class="colorlib-footer">
+                <h3>Follow Us Here!</h3>
+                <div class="d-flex justify-content-center">
+                    <ul class="d-flex align-items-center">
+                        <li class="d-flex align-items-center jusitfy-content-center"><a href="#"><i
+                                        class="icon-facebook"></i></a></li>
+                        <li class="d-flex align-items-center jusitfy-content-center"><a href="#"><i
+                                        class="icon-twitter"></i></a></li>
+                        <li class="d-flex align-items-center jusitfy-content-center"><a href="#"><i
+                                        class="ifigurefigurefigurcon-instagram"></i></a></li>
+                        <li class="d-flex align-items-center jusitfy-content-center"><a href="#"><i
+                                        class="icon-linkedin"></i></a></li>
+                    </ul>
+                </div>
             </div>
-        </div>
     </aside>
     <div id="colorlib-main">
         <section class="ftco-section-2">
-            <?php $form = ActiveForm::begin(['options'=>['id'=>'applicationForm']]); ?>
 
-                        <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
-                        
-                        <?= $form->field($model, 'tell') ?>
-                        
-                        <?= $form->field($model, 'text')->textarea(['rows' => 6]) ?>
-                        
-                        <div class="form-group">
-                            <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
-                        </div>
+			<?php
+                Modal::begin([
+                    'toggleButton' => [
+                        'style' => 'background-image: url(images/image_1.jpg)',
+                        'class' => 'photography-entry imп image-popup d-flex justify-content-center align-items-center',
+                    ],
+                ]);
+			?>
+
+                <?php $form = ActiveForm::begin(['options' => ['id' => 'applicationForm']]); ?>
+
+                    <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
+                    <?= $form->field($model, 'tell') ?>
+                    <?= $form->field($model, 'text')->textarea(['rows' => 6]) ?>
+
+                    <div class="form-group">
+                        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
+                    </div>
 
                 <?php ActiveForm::end(); ?>
+
+			<?php Modal::end(); ?>
+
             <div class="photograhy">
                 <div class="row no-gutters">
                     <div class="col-md-4 ftco-animate">
-                        
-						<?php 
-                            Modal::begin([
-                                'header' => '<h2>Отправь заявку на бронь</h2>',
-                                'toggleButton' => ['label' => 'click me'],
-                                'footer' => 'Низ окна',
-                            ]);
-                             
-                            echo 'Say hello...';
-                             
-                            Modal::end();
-                         ?>
-                        
-
                         <a href="images/image_1.jpg"
                            class="photography-entry imп image-popup d-flex justify-content-center align-items-center"
                            style="background-image: url(images/image_1.jpg);">
@@ -89,9 +86,6 @@ use yii\helpers\Html;
                             </div>
                         </a>
                     </div>
-
-
-
                     <div class="col-md-4 ftco-animate">
                         <a href="images/image_2.jpg"
                            class="photography-entry img image-popup d-flex justify-content-center align-items-center"
